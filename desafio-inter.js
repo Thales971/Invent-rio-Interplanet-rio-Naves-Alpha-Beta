7010838c09a6c9990b5c4b35ef48dbde78e5158e
Thales971 <thales.t.silva6@aluno.senai.br>
2025-06-06 08:47:17 -0300

    feat removendo a ultima tarefa no inventariobeta no arquivo desafio-inter.js

--- desafio-inter.js
+++ desafio-inter.js
@@ -1,3 +1,4 @@
+// 1
 let inventarioAlpha = [
     ["Cristal Rubion", "Plutão", 4500],
     ["Cápsula de Éter", "Júpiter", 7800],
@@ -39,3 +40,50 @@ console.log(inventarioAlpha);
 
 console.log
 ("Estilo Removido:", estiloRemovido)
+console.log("----------------------------------");
+
+//2
+let inventarioBeta = [
+    ["Lâmina Solar", "Mercúrio", 6700],
+    ["Cometa Engarrafado", "Netuno", 7600],
+    ["Relíquia de Zordon", "Elysium", 25000],
+    ["Pó Lunar", "Lua", 2900],
+    ["Nevoeiro Encapsulado", "Júpiter", 3800],
+    ["Chip de Matéria", "Ceres", 4100],
+    ["Chave de Portais", "Júpiter", 5900],
+    ["Globo de Plasma", "Terra", 7200],
+    ["Mapa Estelar", "Vênus", 2100],
+    ["Pedra Temporal", "Lua", 7800],
+    ["Lanterna Estelar", "Sol", 3200],
+    ["Fragmento de Eclipse", "Eclipse E-9", 8300],
+    ["Ponte de Gelo", "Hoth", 3900],
+    ["Fluido de Saturno", "Saturno", 4400],
+    ["Fenda de Tempo", "Marte", 8700],
+    ["Condutor Fotônico", "Sol", 3600],
+    ["Memória Galáctica", "Terra", 5500],
+    ["Livro de Nebulosa", "Andrômeda", 9200],
+    ["Túnel Quântico", "Netuno", 4100],
+    ["Canhão de Luz", "Vênus", 6800],
+    ["Orbe Incandescente", "Sol", 9300],
+    ["Matriz de Propulsão", "Vênus", 4500],
+    ["Partícula Livre", "Urano", 5400],
+    ["Capa de Invisibilidade", "Terra", 3900],
+    ["Terminal de Dobra", "Alpha Centauri", 10500]
+  ];
+  console.log
+  ("Antes do pop");
+  console.table(inventarioBeta);
+  
+  // Removendo ultimo estilo
+  let estiloRemovidoo = inventarioBeta.pop();
+  
+  console.log("Depois do pop");
+  console.log(inventarioBeta);
+  
+  console.log
+  ("Estilo Removido:", estiloRemovidoo) 
+  
+  console.log("--------------------------");
+  
+  
+
